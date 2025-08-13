bbf0e26bb519307f9d5a9505145a632e14b7d07a
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-08-13 20:02:06 +0200

    Add Update endpoint to Regions

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -85,6 +85,33 @@ namespace NZWalks.API.Controllers
 
 			return CreatedAtAction(nameof(GetById), new { id = regionDomainModel.Id}, regionDto);
 		}
+		[HttpPut]
+		[Route("{id:Guid}")]
+		public IActionResult Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
+		{
+			var regionDomainModel = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+			if (regionDomainModel == null)
+			{
+				return NotFound();
+			}
+
+			regionDomainModel.Code = updateRegionRequestDto.Code;
+			regionDomainModel.Name = updateRegionRequestDto.Name;
+			regionDomainModel.RegionImageUrl = updateRegionRequestDto.RegionImageUrl;
+
+			dbContext.SaveChanges();
+
+			var regionDto = new RegionDto
+			{
+				Id = regionDomainModel.Id,
+				Code = regionDomainModel.Code,
+				Name = regionDomainModel.Name,
+				RegionImageUrl = regionDomainModel.RegionImageUrl,
+			};
+
+			return Ok(regionDto);
+		}
 	}
 }
  
