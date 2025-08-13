304b05d906c27d6a4cafd872b9accd0112387a1b
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-08-13 19:07:13 +0200

    Add Create endpoint for Regions

--- NZWalks.API/Controllers/RegionsController.cs
+++ NZWalks.API/Controllers/RegionsController.cs
@@ -61,6 +61,30 @@ namespace NZWalks.API.Controllers
 
 			return Ok(regionDto);
 		}
+		[HttpPost]
+		public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+		{
+			var regionDomainModel = new Region
+			{
+				Code = addRegionRequestDto.Code,
+				Name = addRegionRequestDto.Name,
+				RegionImageUrl = addRegionRequestDto.RegionImageUrl
+			};
+
+			dbContext.Regions.Add(regionDomainModel);
+			dbContext.SaveChanges();
+
+			// Map Domain model back to DTO
+			var regionDto = new RegionDto
+			{
+				Id = regionDomainModel.Id,
+				Code = regionDomainModel.Code,
+				Name = regionDomainModel.Name,
+				RegionImageUrl = regionDomainModel.RegionImageUrl,
+			};
+
+			return CreatedAtAction(nameof(GetById), new { id = regionDomainModel.Id}, regionDto);
+		}
 	}
 }
  
