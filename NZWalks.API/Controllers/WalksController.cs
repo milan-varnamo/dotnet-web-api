8ec7110d3acc6be0b4e9bb062bf434840c3f55c3
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-08-14 23:10:48 +0200

    Add filtering support to GetAll in WalksController

--- NZWalks.API/Controllers/WalksController.cs
+++ NZWalks.API/Controllers/WalksController.cs
@@ -30,9 +30,10 @@ namespace NZWalks.API.Controllers
 				return Ok(mapper.Map<WalkDto>(walkDomainModel));
 		}
 		[HttpGet]
-		public async Task<IActionResult> GetAll()
+		// GET: /api/walks?filterOn=Name&filterQuery=Track
+		public async Task<IActionResult> GetAll([FromQuery] string? filterOn, [FromQuery] string? filterQuery)
 		{
-			var walksDomainModel = await walkRepository.GetAllAsync();
+			var walksDomainModel = await walkRepository.GetAllAsync(filterOn, filterQuery);
 
 			return Ok(mapper.Map<List<WalkDto>>(walksDomainModel));
 		}
