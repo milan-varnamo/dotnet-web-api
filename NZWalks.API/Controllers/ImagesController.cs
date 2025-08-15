f7c2d62227f8b467c85871841b8a4495955caea2
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-08-16 00:00:18 +0200

    Implement image upload functionality

--- NZWalks.API/Controllers/ImagesController.cs
+++ NZWalks.API/Controllers/ImagesController.cs
@@ -1,6 +1,10 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing.Constraints;
+using NZWalks.API.Models.Domain;
 using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
 
 namespace NZWalks.API.Controllers
 {
@@ -8,7 +12,12 @@ namespace NZWalks.API.Controllers
 	[ApiController]
 	public class ImagesController : ControllerBase
 	{
+		private readonly IImageRepository imageRepository;
 
+		public ImagesController(IImageRepository imageRepository)
+		{
+			this.imageRepository = imageRepository;
+		}
 		// POST: /api/Images/Upload
 		[HttpPost]
 		[Route("Upload")]
@@ -17,8 +26,20 @@ namespace NZWalks.API.Controllers
 			ValidateFileUpload(request);
 
 			if (ModelState.IsValid) 
-			{ 
-			
+			{
+				// convert DTO to Domain Model
+				var imageDomainModel = new Image
+				{
+					File = request.File,
+					FileExtension = Path.GetExtension(request.File.FileName),
+					FileSizeInBytes = request.File.Length,
+					FileName = request.FileName,
+					FileDescription = request.FileDescription
+				};
+
+				await imageRepository.Upload(imageDomainModel);
+
+				return Ok(imageDomainModel);
 			}
 
 			return BadRequest(ModelState);
